0872a26f3e35af86605e4bf693e24876be59aa1b
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-02-21 13:00:13 +0100

    Complete webpack build chain for demo

---
+++ dist/h5p-sdk.js
@@ -0,0 +1,334 @@
+// [AIV] Build version: 0.1.0 
+ /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	// Load library
+	H5P = H5P || {};
+	H5P.sdk = H5P.sdk || {};
+	H5P.sdk.panel = __webpack_require__(1).default;
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.default = init;
+	
+	var _elements = __webpack_require__(2);
+	
+	/**
+	 * @type {function}
+	 */
+	var selectExpandable = (0, _elements.querySelector)('[aria-expanded]');
+	
+	/**
+	 * @type {function}
+	 */
+	var isExpanded = (0, _elements.attributeEquals)('aria-expanded', 'true');
+	
+	/**
+	 * Initializes a panel
+	 *
+	 * @param {HTMLElement} element
+	 * @return {HTMLElement}
+	 */
+	function init(element) {
+	  var titleElement = selectExpandable(element);
+	
+	  if (titleElement) {}
+	
+	  return element;
+	}
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.querySelector = exports.attributeEquals = exports.hasAttribute = exports.removeAttribute = exports.setAttribute = exports.getAttribute = undefined;
+	
+	var _functional = __webpack_require__(3);
+	
+	/**
+	 * Get an attribute value from element
+	 *
+	 * @param {string} name
+	 * @param {HTMLElement} el
+	 *
+	 * @function
+	 * @return {string}
+	 */
+	var getAttribute = exports.getAttribute = (0, _functional.curry)(function (name, el) {
+	  return el.getAttribute(name);
+	});
+	
+	/**
+	 * Set an attribute on a html element
+	 *
+	 * @param {string} name
+	 * @param {string} value
+	 * @param {HTMLElement} el
+	 *
+	 * @function
+	 */
+	var setAttribute = exports.setAttribute = (0, _functional.curry)(function (name, value, el) {
+	  el.setAttribute(name, value);
+	});
+	
+	/**
+	 * Remove attribute from html element
+	 *
+	 * @param {string} name
+	 * @param {HTMLElement} el
+	 *
+	 * @function
+	 */
+	var removeAttribute = exports.removeAttribute = (0, _functional.curry)(function (name, el) {
+	  el.removeAttribute(name);
+	});
+	
+	/**
+	 * Check if element has an attribute
+	 *
+	 * @param {string} name
+	 * @param {HTMLElement} el
+	 *
+	 * @function
+	 * @return {boolean}
+	 */
+	var hasAttribute = exports.hasAttribute = (0, _functional.curry)(function (name, el) {
+	  return el.hasAttribute(name);
+	});
+	
+	/**
+	 * Check if element has an attribute that equals
+	 *
+	 * @param {string} name
+	 * @param {string} value
+	 * @param {HTMLElement} el
+	 *
+	 * @function
+	 * @return {boolean}
+	 */
+	var attributeEquals = exports.attributeEquals = (0, _functional.curry)(function (name, value, el) {
+	  return el.getAttribute(name) === value;
+	});
+	
+	/**
+	 * Check if element has an attribute that equals
+	 *
+	 * @param {string} name
+	 * @param {string} value
+	 * @param {HTMLElement} el
+	 *
+	 * @function
+	 * @return {boolean}
+	 */
+	var querySelector = exports.querySelector = (0, _functional.curry)(function (selector, el) {
+	  return el.querySelector(selector);
+	});
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Returns a curried version of a function
+	 *
+	 * @param {function} fn
+	 *
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var curry = exports.curry = function curry(fn) {
+	  var arity = fn.length;
+	
+	  return function f1() {
+	    var args = Array.prototype.slice.call(arguments, 0);
+	    if (args.length >= arity) {
+	      return fn.apply(null, args);
+	    } else {
+	      return function f2() {
+	        var args2 = Array.prototype.slice.call(arguments, 0);
+	        return f1.apply(null, args.concat(args2));
+	      };
+	    }
+	  };
+	};
+	
+	/**
+	 * Compose functions together, executing from right to left
+	 *
+	 * @param {function...} fns
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var compose = exports.compose = function compose() {
+	  for (var _len = arguments.length, fns = Array(_len), _key = 0; _key < _len; _key++) {
+	    fns[_key] = arguments[_key];
+	  }
+	
+	  return fns.reduce(function (f, g) {
+	    return function () {
+	      return f(g.apply(undefined, arguments));
+	    };
+	  });
+	};
+	
+	/**
+	 * Applies a function to each element in an array
+	 *
+	 * @param {function} fn
+	 * @param {Array} arr
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var forEach = exports.forEach = curry(function (fn, arr) {
+	  arr.forEach(fn);
+	});
+	
+	/**
+	 * Maps a function to an array
+	 *
+	 * @param {function} fn
+	 * @param {Array} arr
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var map = exports.map = curry(function (fn, arr) {
+	  return arr.map(fn);
+	});
+	
+	/**
+	 * Applies a filter to an array
+	 *
+	 * @param {function} fn
+	 * @param {Array} arr
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var filter = exports.filter = curry(function (fn, arr) {
+	  return arr.filter(fn);
+	});
+	
+	/**
+	 * Applies a some to an array
+	 *
+	 * @param {function} fn
+	 * @param {Array} arr
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var some = exports.some = curry(function (fn, arr) {
+	  return arr.some(fn);
+	});
+	
+	/**
+	 * Returns true if an array contains a value
+	 *
+	 * @param {*} value
+	 * @param {Array} arr
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var contains = exports.contains = curry(function (value, arr) {
+	  return arr.indexOf(value) != -1;
+	});
+	
+	/**
+	 * Returns an array without the supplied values
+	 *
+	 * @param {Array} values
+	 * @param {Array} arr
+	 *
+	 * @function
+	 * @public
+	 *
+	 * @return {function}
+	 */
+	var without = exports.without = curry(function (values, arr) {
+	  return filter(function (value) {
+	    return !contains(value, values);
+	  }, arr);
+	});
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=h5p-sdk.js.map 
---
+++ src/entries/dist.js
@@ -0,0 +1,5 @@
+// Load library
+H5P = H5P || {};
+H5P.sdk = H5P.sdk || {};
+H5P.sdk.initPanel = require('../scripts/components/panel').default;
+H5P.sdk.initTabPanel = require('../scripts/components/tab-panel').default;
