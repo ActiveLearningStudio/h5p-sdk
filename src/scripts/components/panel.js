808c721a75932d0678e8fc3088d6b0bd6044b413
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-03-01 16:45:01 +0100

    Fix so that panel rendered from dom can be expanded by default

--- src/scripts/components/panel.js
+++ src/scripts/components/panel.js
@@ -23,13 +23,13 @@ const show = setAttribute('aria-hidden', 'false');
  * @param {boolean} isExpanded
  */
 const toggleBodyVisibility = function(bodyElement, isExpanded) {
-  if(isExpanded) {
-    show(bodyElement);
-    bodyElement.style.height = `${bodyElement.scrollHeight}px`;
-  }
-  else {
+  if(!isExpanded) {
     hide(bodyElement);
     bodyElement.style.height = "0";
+  }
+  else if(bodyElement.scrollHeight > 0) {
+    show(bodyElement);
+    bodyElement.style.height = `${bodyElement.scrollHeight}px`;
   }
 };
 
